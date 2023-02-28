2c5e2eb9366d09e80c574c81c9f8378f86a7fa7c
AroraSanya <sanya.arora@netsolutions.com>
2023-02-28 10:38:37 +0530

    quesof hacker

---
+++ que2_E.py
@@ -0,0 +1,18 @@
+'''
+Sample Input 0
+
+5
+Sample Output 0
+
+0
+1
+4
+9
+16
+'''
+
+if __name__ == '__main__':
+    n = int(input())
+    for  i in range(n):
+        result=i*i
+        print(result)
